f6b3cee53e72f7bf9208a7d0994bc845bc3079e0
Macmanuel Olawale Odumeru <65433887+XenoCode54@users.noreply.github.com>
2021-09-20 17:26:36 +0100

    responsive fixes

--- src/components/topContactBar/TopContactBar.jsx
+++ src/components/topContactBar/TopContactBar.jsx
@@ -9,21 +9,25 @@ function TopContactBar (props) {
   return (
     <div className="top">
       <div className="top-contact-bar-body container">
-        <div className="top-contact-bar-text-container">
-          <MailOutlineIcon className="icon" />
-          <p className="top-contact-bar-text">agoruafrancis@gmail.com</p>
+        <div className="mob">
+          <div className="top-contact-bar-text-container">
+            <PhoneIcon className="icon" />
+            <p className="top-contact-bar-text">234(816)1991217</p>
+          </div>
+          <div className="top-contact-bar-text-container">
+            <InstagramIcon className="icon" />
+            <p className="top-contact-bar-text">IG: bitcoindoctor1</p>
+          </div>
         </div>
-        <div className="top-contact-bar-text-container">
-          <PhoneIcon className="icon" />
-          <p className="top-contact-bar-text">234(816)1991217</p>
-        </div>
-        <div className="top-contact-bar-text-container">
-          <FacebookIcon className="icon" />
-          <p className="top-contact-bar-text">FB: Bitcoin Doctor Exchange</p>
-        </div>
-        <div className="top-contact-bar-text-container">
-          <InstagramIcon className="icon" />
-          <p className="top-contact-bar-text">IG: bitcoindoctor1</p>
+        <div className="mob">
+          <div className="top-contact-bar-text-container">
+            <MailOutlineIcon className="icon" />
+            <p className="top-contact-bar-text">agoruafrancis@gmail.com</p>
+          </div>
+          <div className="top-contact-bar-text-container">
+            <FacebookIcon className="icon" />
+            <p className="top-contact-bar-text">FB: Bitcoin Doctor Exchange</p>
+          </div>
         </div>
       </div>
     </div>
